910110de10f470689c38ba82ae869793e6cf7f6c
jayzeezhang <jayzeezhang@jayzeezhangdeMacBook-Pro.local>
2024-07-23 16:37:11 +0800

    redirect

--- next.config.js
+++ next.config.js
@@ -1,14 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    async redirects() {
-        return [
-          {
-            source: '/agent-infomation',
-            destination: 'https://zenless.gg/characters/ellen/',
-            statusCode: 302
-          },
-        ]
-      }
+    // async redirects() {
+    //     return [
+    //       {
+    //         source: '/agent-infomation',
+    //         destination: 'https://zenless.gg/characters/ellen/',
+    //         statusCode: 302
+    //       },
+    //     ]
+    //   }
 }
 
 module.exports = nextConfig
